dc47c37693640e587e349df20842c6d0f39805a6
AdamGoris <adam.goris@students.mq.edu.au>
2019-09-19 15:01:04 +1000

    Added imports for CParser

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,11 +7,16 @@ object Main extends Messaging with PositionStore {
     import whiley.WhileyI
     import whiley.WhileyIPrettyPrinter._
     import whiley.WhileyISyntax.Program
+    import whiley.CI
+    //import whiley.CIPrettyPrinter._
+    import whiley.CISyntax.CProgram
     import org.bitbucket.inkytonik.kiama.util.FileSource
 
     def main(args : Array[String]) {
         val p = new WhileyI(FileSource(args(0)), positions)
+        //val p = new CI(FileSource(args(0)), positions)
         val pr = p.pProgram(0)
+        println(pr)
         if (pr.hasValue) {
             val v = p.value(pr).asInstanceOf[Program]
             println(show(v, 1))
@@ -21,5 +26,4 @@ object Main extends Messaging with PositionStore {
             println(s"parse failed\n${formatMessage(m)}")
         }
     }
-
 }
