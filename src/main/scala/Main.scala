794ba0038928602d00a7918f440e1bdca3856af3
AdamGoris <adam.goris@students.mq.edu.au>
2019-09-23 18:47:06 +1000

    Added a way to print output of the translator to the terminal.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,4 @@
-package demo
+package runtime
 
 import org.bitbucket.inkytonik.kiama.util.{Messaging, PositionStore}
 
@@ -11,16 +11,18 @@ object Main extends Messaging with PositionStore {
     //import whiley.CIPrettyPrinter._
     import whiley.CISyntax.CProgram
     import org.bitbucket.inkytonik.kiama.util.FileSource
+    import translator.Translator
 
     def main(args : Array[String]) {
         val p = new WhileyI(FileSource(args(0)), positions)
+        val translator = new Translator
         //val p = new CI(FileSource(args(0)), positions)
         val pr = p.pProgram(0)
-        println(pr)
         if (pr.hasValue) {
             val v = p.value(pr).asInstanceOf[Program]
             println(show(v, 1))
             println(pretty(any(v)).layout)
+            println(translator.translate(v))
         } else {
             val m = p.errorToMessage(pr.parseError)
             println(s"parse failed\n${formatMessage(m)}")
